747cdedcf7e9dc658fb2f36c87abe0de799ae22b
Riquelmy Arcanjo de Araújo <riquelmyaaraujo@gmail.com>
2025-01-16 02:14:26 -0300

    Alteração do caminho de imagens dinamicamente, novas fontes e estilização de componentes

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +9,16 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app';
+
+  imgPath: string | undefined;
+
+  ngOnInit() {}
+
+  getImagePath(imageName: string): string {
+    const basePath = window.location.hostname === 'localhost' ? '' : '/aniversario-50-anos-arlete';
+    return `${basePath}/${imageName}`;
+  }
+
 }
