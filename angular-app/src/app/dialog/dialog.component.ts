c507da95ce2f28feeec98b1834ba3a16e655ef4e
Riquelmy Arcanjo de Araújo <riquelmyaaraujo@gmail.com>
2025-01-17 12:05:28 -0300

    Fechar modal, mensagem agradecimento, novas mídias

--- angular-app/src/app/dialog/dialog.component.ts
+++ angular-app/src/app/dialog/dialog.component.ts
@@ -1,18 +1,18 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSnackBarModule]
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSnackBarModule, MatDialogModule]
 })
 export class DialogComponent {
   constructor(private snackBar: MatSnackBar, public dialogRef: MatDialogRef<DialogComponent>,
@@ -26,6 +26,22 @@ export class DialogComponent {
       });
     }, (err) => {
       console.error('Erro ao copiar o texto: ', err);
+    });
+  }
+
+  triggerConfetti() {
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 }
+    });
+  }
+
+  closeDialog() {
+    this.triggerConfetti();
+    this.dialogRef.close();
+    this.snackBar.open('Se deu tudo certo em seu banco tenho certeza que o presente chegou em tempo recorde! Obrigado, ela irá amar 🎉🥳', '', {
+      duration: 10000,
     });
   }
 }
